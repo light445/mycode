f1086820dbb2bbd12ee6bcfcba24e711d79a0f3d
light445 <2523796786@qq.com>
2025-03-12 19:03:40 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,90 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using mycodelib;
-
-namespace first
+using System.Runtime.DesignerServices;
+using System.Xml.Linq;
+//命名空间声明
+namespace HellowWordCS
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+    //类定义：public class 类名{
+    class Program
+    {//只能有一个main方法
+        static void Main()
         {
-            Console.WriteLine("月黑风高杀人夜");
-            Console.WriteLine("请按任意键继续");
-            Console.ReadKey();
-
-            Console.WriteLine("微风吹动这路边的小草");
-            Console.WriteLine("请按任意键继续");
-            Console.ReadKey();
-
-            Console.WriteLine("隐约在路的尽头有一个人影");
-            Console.WriteLine("请按任意键继续");
-            Console.ReadKey();
-
-            Console.WriteLine("走进一看这人叫");
-            Console.WriteLine("请按任意键继续");
-            Console.ReadKey();
-
-
-            Console.WriteLine("请输入大侠的名字：");
-            string name = Console.ReadLine();
-            Console.WriteLine("原来你就是" + name);
-            Console.WriteLine("请按任意键继续");
-            Console.ReadKey();
-
-            Hero daxia = new Hero();
-            daxia.xingbie = "男";
-            daxia.age = 25;
-            daxia.shanghai = 10000;
-            daxia.hp = 100;
-            daxia.shenggao = 180;
-            daxia.jineng1 = "战斗力强";
-            daxia.jineng1shanghai = 200;
-            daxia.jineng2 = "智力超群";
-            daxia.jineng2shanghai = 150;
-            //ShowInfo showInfo = new ShowInfo();
-            ShowInfo.ShowHeroInfo(daxia);
-
-
-
-
-
-            Console.WriteLine("此处传来了一个女人的声音");
-            Console.WriteLine("请按任意键继续");
-            Console.ReadKey();
-
-            Console.WriteLine("请输入女侠的名字：");
-
-
-            string name2 = Console.ReadLine();
-            Console.WriteLine("原来你叫" + name2);
-            Console.WriteLine("请按任意键继续");
-            Console.ReadKey();
-
-            Hero nvxia = new Hero();
-
-            nvxia.xingbie = "女";
-            nvxia.age = 20;
-            nvxia.shanghai = 5000;
-            nvxia.hp = 80;
-            nvxia.shenggao = 160;
-            nvxia.jineng1 = "魅力十足";
-            nvxia.jineng1shanghai = 100;
-            nvxia.jineng2 = "幽默感十足";
-            nvxia.jineng2shanghai = 120;
-            //将Hero类中创建的信息传递给ShowInfo类中的ShowHeroInfo方法
-            
-            ShowInfo.ShowHeroInfo(nvxia);
-
-
-
-
+            GetInput();
 
 
         }
+        //方法定义：static 方法类型  方法名(参数类型 参数名) -->无返回值类型
+        //有返回值方法定义：
+        //void为空值
+        //static(私有)
+        //public(公共)
+        static void GetInput()
+        {
+            Console.WriteLine("请输入您的姓名：");
+            string name = Console.ReadLine();
+            Console.WriteLine("请输入您的年龄：");
+            int age = int.Parse(Console.ReadLine());
+            //try
+            //{
+            //    Console.WriteLine("请输入您的身高：");
+            //    double height = double.Parse(Console.ReadLine());
+
+            name = ChangeDate(name);
+           
+
+
+
+
+            Console.WriteLine("你的年龄是"+age);
+        }
+        //方法定义：static 方法类型  方法名(参数类型 参数名) -->返回值类型
+        //方法调用：方法类型.方法名(参数)
+        static string ChangeDate(string name1)
+        {
+            if (name1 == "张三")
+            {
+                Console.WriteLine("你好,法外狂徒张三");
+            }
+            else
+            {
+                Console.WriteLine("你好, " + name1);
+            }
+
+            return name1;
+        }
+
+
     }
-    
-    
+
+     class Hero
+    {
+        public int hp=100;
+        public int atk;
+        public int def;
+        
+        static void Get()
+        {
+            Console.WriteLine("hp");
+        }
+
+    }
 }
