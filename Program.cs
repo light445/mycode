f9e4251488dc681f94823afa8a9fba846287bfba
light445 <2523796786@qq.com>
2025-03-16 20:05:49 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,33 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace lianxiGAME1
+
+namespace 日常20253._16
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("请输入你的名字：");
             string name = (Console.ReadLine());
-            Console.WriteLine("请输入你的年龄：");
-            int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("你好" + name);
-            if (age == 18)
-            {
-                Console.WriteLine("恭喜你成年了");
+            Console.WriteLine("你好，"+name);
 
-            }
-            else if(age < 18)
-            {
-                Console.WriteLine("你还未成年");
-            }
-            else 
-            {
-                Console.WriteLine("你今年" + age + "岁了");
-            }
-            
-            
             Console.ReadKey();
+
         }
     }
 }
