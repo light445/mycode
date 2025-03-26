e70d9b1e6ddfd0f96805e66b1bacdd308694374d
light445 <2523796786@qq.com>
2025-03-26 19:34:16 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -13,53 +13,26 @@ namespace 日常
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("今天是2025.3.15");
-            Hero xiaoli = new Hero();
-            
-            xiaoli.name = "李探花";
-            xiaoli.hp = 100;
-            xiaoli.attack = 100;
-            xiaoli.defence = 100;
-            xiaoli.speed = 100;
-            xiaoli.XingWi();
-
-
-        }
-        /* static void ChangeName()
-         {
-             Console.WriteLine("请输入你的名字：");
-             string name = Console.ReadLine();
-             if (name == "小李")
-             {
-                 Console.WriteLine("你好李探花");
-             }
-             else
-             {
-                 Console.WriteLine("你好，" + name);
-             }
-         }
-     }*/
-        class Hero
-        {
-            public string name;
-            public int hp;
-            public int attack;
-            public int defence;
-            public int speed;
-            public void XingWi()
+            Console.WriteLine("今天是2025.3.26");
+           
+            for (int i = 0 ; ; i++)
             {
-                Console.WriteLine("技能1：小李飞刀");
-                Console.WriteLine("技能2：星位移");
-                Console.WriteLine("技能3：星位斩");
-                Console.WriteLine("技能4：星位爆");
-                Console.WriteLine("你的名字：" + name);
-                Console.WriteLine("你的速度：" + speed);
-                Console.WriteLine("你的防御力：" + defence);
-                Console.WriteLine("你的血量：" + hp);
-                Console.WriteLine("你的攻击力：" + attack);
-
+                Console.WriteLine("请输入你的名字");
+                string name=Console.ReadLine();
+                if(name=="李白")
+                Console.WriteLine("你好，李太白");
+                else
+                {
+                    Console.WriteLine("你好，"+name);
+                } 
                 
             }
+
+         
+            
+
         }
+       
+        
     }
 }
